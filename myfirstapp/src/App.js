6b8edbc25b4aa55930ae7b13e743d436f3fa2b8c
AshenGod <jacobrawefi122@outlook.com>
2022-08-08 11:01:25 +0100

    ya3

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Forum  from './Components/Forum'
+import NavBar from './Components/Nav'
+
+function App() {
+  return (
+    <>
+
+    <NavBar/>
+    <Forum/>
+
+    </>
+    
+  );
+}
+
+export default App;
